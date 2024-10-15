3465bc0a57d99a78dd1fa5f7288b768b2794be77
vinicius <niviciusdev@gmail.com>
2024-10-14 23:37:40 -0300

    feat: directory structure and added django to venv project

---
+++ mkdir_structure.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# nao precisa rodar novamente.
+# deixei aqui caso necessario modificar a estrutura toda xD
+mkdir -p ft_transcendence/{backend/{django/{api,authentication,microservices,project_name},database/postgresql/{migrations,seeds}},frontend/{js/{game,spa,threejs},styles},infrastructure/{docker/{backend,frontend},ci-cd/github-actions/{secrets}},monitoring/{prometheus,grafana/dashboards},logging/elk-stack/{elasticsearch,logstash,kibana},docs/{architecture,api},project-management/{github-projects,tasks,reports},security/{waf-modsecurity,vault,audit}}
+
